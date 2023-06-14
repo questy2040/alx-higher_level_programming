5058e1c0684c95bdfe441cc0a63890dab9f5b15c
john <john bosco>
2023-06-14 16:22:28 +0000

    javascript

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle
 """
-Defines a class Rectangle
-"""
-
-
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
@@ -12,12 +9,10 @@ class Rectangle:
         """
         self.height = height
         self.width = width
-
     @property
     def width(self):
         """getter for the private instance attribute width"""
         return self.__width
-
     @width.setter
     def width(self, value):
         """setter for the private instance attribute width"""
@@ -26,12 +21,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
     @property
     def height(self):
         """getter for the private instance attribute height"""
         return self.__height
-
     @height.setter
     def height(self, value):
         """setter for the private instance attribute height"""
@@ -40,4 +33,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,18 +3,17 @@
 N queens backtracking program to print the coordinates of N queens
 on an NxN grid such that they are all in non-attacking positions
 """
-
-
 from sys import argv
-
 if __name__ == "__main__":
     a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
+
     if argv[1].isdigit() is False:
         print("N must be a number")
         exit(1)
+
     n = int(argv[1])
     if n < 4:
         print("N must be at least 4")
